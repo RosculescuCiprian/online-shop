4d8040fd7cbf37ba400e9a201c2a6f679b33e288
RosculescuCiprian <rosculescu.ciprian.daniel@gmail.com>
2018-03-08 15:16:47 +0200

    Case class and table representation

--- app/models/sellers/Seller.scala
+++ app/models/sellers/Seller.scala
@@ -1,5 +1,12 @@
 package models.sellers
 
-class Seller {
+import play.api.libs.json.{Json, OFormat}
 
+case class Seller(id: Long, name: String, desc: String) {
+  def patch(name: Option[String] = None, desc: Option[String] = None): Seller =
+    this.copy(name = name.getOrElse(this.name), desc = desc.getOrElse(this.desc))
+}
+
+object Seller {
+  implicit val sellerFormat:OFormat[Seller] = Json.format[Seller]
 }
